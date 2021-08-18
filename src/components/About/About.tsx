cc3360c95885d68648034d9e8029bbd42612acbc
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-18 10:47:57 +0530

    style: Fixed the responsive issue with about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,22 +3,22 @@ const About = () => {
   return (
     <div className="bcard mid-section">
       <header className="head">
-        <h1>Learning Recommendation</h1>
+        <h1>About</h1>
       </header>
       <section className="about">
-        <h1 id="description">Description</h1>
+        <h2 id="description">Description</h2>
         <p>
           This is a web app that Recommends different learning material based on
           Topics. Built using ReactJS.{" "}
         </p>
-        <h1 id="live-link">Live Link</h1>
+        <h2 id="live-link">Live Link</h2>
         <p>
           <a href="https://github.com/SJTGSHIVAM/learning_recommendation_mark9">
-            https://github.com/SJTGSHIVAM/learning_recommendation_mark9
+            Click here
           </a>
         </p>
-        <h1 id="salient-features-are">Salient features are</h1>
-        <ul>
+        <h2 id="salient-features-are">Salient features are</h2>
+        <ol>
           <li>Built using ReactJS</li>
           <li>Recommending learning resource</li>
           <li>One can choose from different topics as per intrest</li>
@@ -27,7 +27,7 @@ const About = () => {
             Description rating and a link to acess the resorces is provided with
             every recommendation
           </li>
-        </ul>
+        </ol>
       </section>
     </div>
   );
